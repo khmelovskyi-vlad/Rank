ee671c5e6856f3a248509034325e3772b38c6597
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-06-27 21:17:07 +0300

    try catch: some numbers; backSpace, '('=')', don`t have number

--- Rank/Rank/Program.cs
+++ Rank/Rank/Program.cs
@@ -8,6 +8,8 @@ namespace Rank
 {
     class Program
     {
+        const int first = 47;
+        const int end = 58;
         static void Main(string[] args)
         {
             Console.WriteLine("Rang game");
@@ -19,10 +21,15 @@ namespace Rank
         static int Rank ()
         {
             string numAndBracket = ReadString("string with numbers, '(' and ')' ");
-            char num = CharRead("simbol");
+            char num = CharRead("your simbol");
+            Console.WriteLine();
+            char numInString = CharRead("number your simbol in string");
             int rank = 0;
-            for(int i = 0; i < numAndBracket.Length; i++)
+            var havingNum = false;
+            while (true)
             {
+                for (int i = 0; i < numAndBracket.Length; i++)
+                {
                     if (numAndBracket[i] == '(')
                     {
                         rank++;
@@ -31,17 +38,38 @@ namespace Rank
                     {
                         rank--;
                     }
-                    else if (numAndBracket[i] == num)
+                    if (numAndBracket[i] == num && numInString != first + 2)
                     {
+                        numInString--;
+                        continue;
+                    }
+                    else if (numAndBracket[i] == num && numInString == first + 2)
+                    {
+                        havingNum = true;
                         break;
                     }
+                }
+                if (havingNum == false)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Don`t have your simbol in string");
+                    num = CharRead("your new simbol or click escape");
+                    Console.WriteLine();
+                    numInString = CharRead("number your new simbol in string or click escape");
+                    continue;
+                }
+                else
+                {
+                    return rank;
+                }
             }
-            return rank;
         }
         static string ReadString(string readString)
         {
             Console.WriteLine($"Enter {readString}");
             string line = "";
+            var leftBracket = 0;
+            var rightBracket = 0;
             while (true)
             {
                 var key = Console.ReadKey();
@@ -49,19 +77,45 @@ namespace Rank
                 {
                     throw new OperationCanceledException();
                 }
-                else if (key.KeyChar > 47 && key.KeyChar < 58 || key.KeyChar == '(' || key.KeyChar == ')')
+                else if (key.Key == ConsoleKey.Backspace)
                 {
+                    line = line.Remove(line.Length-1);
+                }
+                else if (key.KeyChar > first && key.KeyChar < end)
+                {
+                    line += key.KeyChar;
+                }
+                else if (key.KeyChar == '(')
+                {
+                    leftBracket++;
+                    line += key.KeyChar;
+                }
+                else if (key.KeyChar == ')')
+                {
+                    rightBracket++;
                     line += key.KeyChar;
                 }
                 else if (key.Key == ConsoleKey.Enter)
                 {
-                    Console.WriteLine();
-                    return line;
+                    if (leftBracket == rightBracket)
+                    {
+                        Console.WriteLine();
+                        return line;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Bet input '(' != ')', try again or click escape");
+                        line = "";
+                        leftBracket = 0;
+                        rightBracket = 0;
+                        continue;
+                    }
                 }
                 else
                 {
                     Console.WriteLine();
-                    Console.WriteLine($"Bet input {readString}, try again or click escape");
+                    Console.WriteLine($"Bet input {key.KeyChar}, try again or click escape");
                     line = "";
                     continue;
                 }
@@ -77,7 +131,7 @@ namespace Rank
                 {
                     throw new OperationCanceledException();
                 }
-                else if (key.KeyChar > 47 && key.KeyChar < 58)
+                else if (key.KeyChar > first && key.KeyChar < end)
                 {
                     return key.KeyChar;
                 }
